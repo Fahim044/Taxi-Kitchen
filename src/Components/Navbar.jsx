67edf0aee7a99c6a030495e41930273ee636b0e8
Fahim044 <fahiimfaysalleusc044@gmail.com>
2025-10-03 10:41:04 +0600

    fixed navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <div className='flex flex-col lg:flex-row gap-2 justify-between items-center'>
             <Logo></Logo>
-            <ul className='flex gap-16'>
+            <ul className='flex lg:gap-16 gap-12 '>
                 <li><a href="" className='hover border-amber-500 hover:border-b-3  hover:font-bold'>Orders</a></li>
                 <li><a href="" className='hover border-amber-500 hover:border-b-3  hover:font-bold'>Foods</a></li>
                 <li><a href="" className='hover border-amber-500 hover:border-b-3  hover:font-bold'>Tables</a></li>
